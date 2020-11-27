397c48a49d55ba674cc8942555ce57b0d76b81e5
bunlengEarb <tekdeiplerngkyol018@gmail.com>
2020-11-26 19:45:02 -0600

    new commit

---
+++ HelloWorld1/GreetPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld1
+{
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+
+            slider1.Value = 0.5;
+        }
+
+        void Slider_ValueChanged(System.Object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            label1.Text = String.Format("Value is {0:F2}",e.NewValue);
+        }
+
+    }
+}
